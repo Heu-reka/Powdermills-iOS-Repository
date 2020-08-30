1c85a3657c9ecb8af928b2d5483aef780bf3d6ef
James Sadlier <james.sadlier@over-c.com>
2020-08-30 08:55:27 +0100

    Initial commit

---
+++ Powdermills/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Powdermills
+//
+//  Created by James Sadlier on 30/08/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Powdermills/PowdermillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowdermillsApp.swift
+//  Powdermills
+//
+//  Created by James Sadlier on 30/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct PowdermillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
