b465817f281279c57c84d914d372fab804252da5
James Sadlier <jamiesadlier@gmail.com>
2021-02-21 16:03:06 +0000

    Added t&c popup and storing acceptance in user defaults

--- Powdermills/PowdermillsApp.swift
+++ Powdermills/PowdermillsApp.swift
@@ -11,18 +11,20 @@ import FirebaseCore
 @main
 struct PowdermillsApp: App {
 	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+	@State var notAcceptedTerms: Bool = UserDefaults.standard.bool(forKey: TermsAndConditionsView.ACCEPTED_TERMS) != true
 	
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
+				.fullScreenCover(isPresented: $notAcceptedTerms, content: {
+					TermsAndConditionsView()
+				})
 		}
 	}
 }
 
 class AppDelegate: NSObject, UIApplicationDelegate {
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-		//			FirebaseApp.configure()
-		
 		let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")!
 		let options = FirebaseOptions(contentsOfFile: filePath)
 		FirebaseApp.configure(options: options!)
